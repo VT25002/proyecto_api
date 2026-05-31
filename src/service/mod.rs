208f6ad9038d5778ccfdf0b720cdc2954c671aca
Manuel Saravia <ss25027@ues.edu.sv>
2026-05-30 21:54:22 -0600

    Lógica de Negocio: Creación de capa de servicios para Usuarios

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,4 +1,5 @@
-pub mod prestamos_service; 
 pub mod autores_service;
 pub mod editoriales_service;
 pub mod libros_service;
+pub mod prestamos_service;
+pub mod usuario_service;
---
+++ src/service/usuario_service.rs
@@ -0,0 +1,27 @@
+use crate::models::usuario::{CrearUsuario, Usuario};
+use crate::repository::usuario_repository;
+use sqlx::PgPool;
+
+pub async fn listar_usuarios(pool: &PgPool) -> Result<Vec<Usuario>, sqlx::Error> {
+    usuario_repository::obtener_todos(pool).await
+}
+
+pub async fn obtener_usuario(pool: &PgPool, id: i32) -> Result<Option<Usuario>, sqlx::Error> {
+    usuario_repository::obtener_por_id(pool, id).await
+}
+
+pub async fn crear_usuario(pool: &PgPool, data: CrearUsuario) -> Result<Usuario, sqlx::Error> {
+    usuario_repository::crear(pool, data).await
+}
+
+pub async fn actualizar_usuario(
+    pool: &PgPool,
+    id: i32,
+    data: CrearUsuario,
+) -> Result<Option<Usuario>, sqlx::Error> {
+    usuario_repository::actualizar(pool, id, data).await
+}
+
+pub async fn eliminar_usuario(pool: &PgPool, id: i32) -> Result<u64, sqlx::Error> {
+    usuario_repository::eliminar(pool, id).await
+}
